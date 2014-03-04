9787cba606abc6747dea37623b4d6de7867dad62
Pedro Maltez <maltezpedro@gmail.com>
2014-03-04 07:26:29 +0000

    Generate rails files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LetolabWebDeveloperTest::Application.config.secret_key_base = 'cdddd03ddcf55cb63214081d741a52cb93cc165ab49c6c94fecfb40d55d3032514e3ff8dc5fca5bbd71ca46d1e66fac75af141adeab287aa1dfbf511fca592b7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LetolabWebDeveloperTest::Application.config.session_store :cookie_store, key: '_letolab-web-developer-test_session'
